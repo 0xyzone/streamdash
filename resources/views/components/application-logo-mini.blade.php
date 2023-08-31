12933a75e4fe1e6ecc462f4bdcdb0ca374042fbc
0xyzone <58982560+0xyzone@users.noreply.github.com>
2023-08-31 17:01:56 +0545

    major update
    - updated logo
    - updated UX a bit with sidebar menu updates and Logo manipulation

---
+++ resources/views/components/application-logo-mini.blade.php
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- Generator: Adobe Illustrator 26.0.1, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+<svg {{ $attributes }} version="1.1" id="icon-0" xmlns:svgjs="http://svgjs.dev/svgjs"
+    xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+    viewBox="0 0 1000 1000" style="enable-background:new 0 0 1000 1000;" xml:space="preserve">
+    <style type="text/css">
+        .st0 {
+            fill: none;
+        }
+
+        .st1 {
+            fill-rule: evenodd;
+            clip-rule: evenodd;
+            fill: #FFFFFF;
+        }
+
+        .st2 {
+            fill: #FFFFFF;
+        }
+
+        .st3 {
+            display: none;
+            fill: none;
+        }
+    </style>
+    <g transform="matrix(1,0,0,1,422.8480587504062,0)">
+        <g>
+            <g>
+                <polygon class="st0" points="-518.7,-95.8 673,-95.8 673,1095.8 -518.7,1095.8 			" />
+                <g>
+                    <path class="st1"
+                        d="M-217.9,505.8l46.3,46.3c69.4-69.4,150.4-115.7,254.5-115.7s196.7,46.3,254.5,115.7l46.3-46.3
+					C221.8,332.2-55.9,332.2-217.9,505.8z M-148.5,575.2L82.9,806.6l231.4-231.4c-57.8-57.8-138.8-92.6-231.4-92.6
+					S-90.6,517.4-148.5,575.2z" />
+                    <polygon class="st2"
+                        points="-333.6,158.7 545.7,158.7 545.7,841.3 187.1,841.3 279.6,748.7 453.2,748.7 453.2,251.3 
+					-287.3,251.3 -287.3,748.7 -102.2,748.7 -9.6,841.3 -379.8,841.3 -379.8,158.7 				" />
+                </g>
+            </g>
+        </g>
+    </g>
+    <rect x="151.1" y="425.1" class="st3" width="697" height="150.3" />
+</svg>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,15 +4,10 @@
             <h2 class="text-xl font-semibold leading-tight">
                 {{ __('Dashboard') }}
             </h2>
-            <x-button target="_blank" href="https://github.com/kamona-wd/kui-laravel-breeze" variant="black"
-                class="justify-center max-w-xs gap-2">
-                <x-icons.github class="w-6 h-6" aria-hidden="true" />
-                <span>Star on Github</span>
-            </x-button>
         </div>
     </x-slot>
 
     <div class="p-6 overflow-hidden bg-white rounded-md shadow-md dark:bg-dark-eval-1">
-        {{ __("You're logged in!")  }}
+        {{ __("You're logged in!") }}
     </div>
 </x-app-layout>
